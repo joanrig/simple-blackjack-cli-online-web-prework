eeefe66ba8e13f60ac550594e9599d8cea9cb73c
Joan in DC <joanrigdon@gmail.com>
2018-12-20 00:20:09 +0000

    Automatically backed up by Learn

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,14 +1,3 @@
 require_relative "blackjack.rb"
 
-def runner
-  welcome
-  initial_round
-  
-  until sum > 21 do
-    card_total = hit?(card_total)
-    display_card_total
-  
-  if card_total > 21
-    end_game(card_total)
-  end
-end
+runner
